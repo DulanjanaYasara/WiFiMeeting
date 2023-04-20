54e26048d75ba936ac01a5fccfaa1a6b4c2633f1
Dulanjana.Liyanagama <Dulanjana.Liyanagama@axiatadigitallabs.com>
2023-04-20 23:05:34 +0530

    wifi permission validation

--- app/src/main/java/com/example/wifimeeting/page/StartupPage.java
+++ app/src/main/java/com/example/wifimeeting/page/StartupPage.java
@@ -1,5 +1,7 @@
 package com.example.wifimeeting.page;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,9 +10,10 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.example.wifimeeting.navigation.NavigationHost;
 import com.example.wifimeeting.R;
+import com.example.wifimeeting.navigation.NavigationHost;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
 
 /**
  * Fragment representing the login screen for WifiMeeting.
@@ -29,17 +32,29 @@ public class StartupPage extends Fragment {
         studentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((NavigationHost) getActivity()).navigateTo(new StudentHomePage(), true);
+                if(isWifiConnected(view))
+                    ((NavigationHost) getActivity()).navigateTo(new StudentHomePage(), true);
             }
         });
 
         lecturerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((NavigationHost) getActivity()).navigateTo(new LecturerLoginPage(), true);
+                if(isWifiConnected(view))
+                    ((NavigationHost) getActivity()).navigateTo(new LecturerLoginPage(), true);
             }
         });
 
         return view;
+    }
+
+    public boolean isWifiConnected(View view) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        android.net.NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if ( wifi == null || !wifi.isConnected()) {
+            Snackbar.make(view, R.string.not_wifi_connected, Snackbar.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
