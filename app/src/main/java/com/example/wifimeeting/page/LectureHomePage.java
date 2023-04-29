b55df495ac38241c8b52786b35d10d0833969daf
Dulanjana.Liyanagama <Dulanjana.Liyanagama@axiatadigitallabs.com>
2023-04-29 19:30:07 +0530

    permission check

--- app/src/main/java/com/example/wifimeeting/page/LectureHomePage.java
+++ app/src/main/java/com/example/wifimeeting/page/LectureHomePage.java
@@ -1,5 +1,6 @@
 package com.example.wifimeeting.page;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.format.Formatter;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.wifimeeting.R;
@@ -17,6 +19,7 @@ import com.example.wifimeeting.utils.Constants;
 import com.example.wifimeeting.utils.MyDetails;
 import com.example.wifimeeting.utils.Role;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -52,15 +55,20 @@ public class LectureHomePage extends Fragment {
                joinLectureValidations();
 
                 if(portTextInput.getError() == null && lecturerNameTextInput.getError() == null) {
-                    Bundle bundle = new Bundle();
-                    bundle.putString(MyDetails.NAME.toString(), Objects.requireNonNull(lecturerNameEditText.getText()).toString());
-                    bundle.putBoolean(MyDetails.IS_MUTE.toString(), false);
-                    bundle.putString(MyDetails.ROLE.toString(), Role.LECTURER.toString());
 
-                    MeetingPage meetingPage = new MeetingPage();
-                    meetingPage.setArguments(bundle);
-                    // Navigate to the next Fragment
-                    ((NavigationHost) getActivity()).navigateTo(meetingPage, true);
+                    if (ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+                        Snackbar.make(view, R.string.permission_mandatory, Snackbar.LENGTH_SHORT).show();
+				    else {
+                        Bundle bundle = new Bundle();
+                        bundle.putString(MyDetails.NAME.toString(), Objects.requireNonNull(lecturerNameEditText.getText()).toString());
+                        bundle.putBoolean(MyDetails.IS_MUTE.toString(), false);
+                        bundle.putString(MyDetails.ROLE.toString(), Role.LECTURER.toString());
+
+                        MeetingPage meetingPage = new MeetingPage();
+                        meetingPage.setArguments(bundle);
+                        // Navigate to the next Fragment
+                        ((NavigationHost) getActivity()).navigateTo(meetingPage, true);
+                    }
                 }
             }
         });
--- app/src/main/java/com/example/wifimeeting/page/StudentHomePage.java
+++ app/src/main/java/com/example/wifimeeting/page/StudentHomePage.java
@@ -1,5 +1,6 @@
 package com.example.wifimeeting.page;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.format.Formatter;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.wifimeeting.R;
@@ -17,6 +19,7 @@ import com.example.wifimeeting.utils.Constants;
 import com.example.wifimeeting.utils.MyDetails;
 import com.example.wifimeeting.utils.Role;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -70,15 +73,19 @@ public class StudentHomePage extends Fragment {
                 checkValidations();
                 if(portTextInput.getError() == null && studentNameTextInput.getError() == null) {
 
-                    Bundle bundle = new Bundle();
-                    bundle.putString(MyDetails.NAME.toString(), Objects.requireNonNull(studentNameEditText.getText()).toString());
-                    bundle.putBoolean(MyDetails.IS_MUTE.toString(), true);
-                    bundle.putString(MyDetails.ROLE.toString(), Role.STUDENT.toString());
+                    if (ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+                        Snackbar.make(view, R.string.permission_mandatory, Snackbar.LENGTH_SHORT).show();
+                    else {
+                        Bundle bundle = new Bundle();
+                        bundle.putString(MyDetails.NAME.toString(), Objects.requireNonNull(studentNameEditText.getText()).toString());
+                        bundle.putBoolean(MyDetails.IS_MUTE.toString(), true);
+                        bundle.putString(MyDetails.ROLE.toString(), Role.STUDENT.toString());
 
-                    MeetingPage meetingPage = new MeetingPage();
-                    meetingPage.setArguments(bundle);
-                    // Navigate to the next Fragment
-                    ((NavigationHost) getActivity()).navigateTo(meetingPage, true);
+                        MeetingPage meetingPage = new MeetingPage();
+                        meetingPage.setArguments(bundle);
+                        // Navigate to the next Fragment
+                        ((NavigationHost) getActivity()).navigateTo(meetingPage, true);
+                    }
                 }
 
             }
